8afa04debbf00f1afaa6c8e59042d873d2bfad2a
Alexander Joshua <avj1@williams.edu>
2021-03-08 02:37:24 +0000

    Add form for people to contact me.

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -9,17 +9,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
-  @Override
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6734233536872851287L;
+
+    @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    String emailValue = request.getParameter("email-input");
+    String messageValue = request.getParameter("message-input");
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println("You submitted: " + emailValue + " " + messageValue);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println("You submitted: " + emailValue + " " + messageValue);
     response.sendRedirect("/");
   }
 }
