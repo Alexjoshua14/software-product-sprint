4dee8c4a1fd42c7911bb3ad422f4c62fbceafdf6
Alexander Joshua <avj1@williams.edu>
2021-03-08 02:52:15 +0000

    Add form for people to contact me.

--- portfolio/src/main/java/com/google/sps/servlets/HelloServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloServlet.java
@@ -21,7 +21,7 @@ public class HelloServlet extends HttpServlet {
 
     @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    ArrayList<String> hello = new ArrayList<String>();
+    ArrayList<String> hello = new ArrayList<>();
     hello.add("Hej Hej!");
     hello.add("Hello!");
     hello.add("Hola");
